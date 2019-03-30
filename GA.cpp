d47e8fb28ee01baabe23e16bf46e2c9debe01a43
Kelsey Kelley <kelseykelley@kelseymacbookpro.nomads.utk.edu>
2019-03-30 19:05:26 -0400

    Started main program

---
+++ GA.cpp
@@ -0,0 +1,15 @@
+//
+//  GA.cpp
+//  
+//
+//  Created by Kelsey Kelley on 3/30/19.
+//
+
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[]){
+    
+}
